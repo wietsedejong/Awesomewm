82f92cc85af3520e87971f769f3da837eda1d601
Wietse de Jong <info@wietsedejong.nl>
2020-08-18 22:44:20 +0200

    startup change

--- autostart.sh
+++ autostart.sh
@@ -6,7 +6,7 @@ function run {
     $@&
   fi
 }
-run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
+#run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 run "nm-applet"
@@ -19,7 +19,7 @@ run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 run "numlockx on"
 run "volumeicon"
 #run "nitrogen --restore"
-#run "conky -c $HOME/.config/awesome/system-overview"
+run "conky -c $HOME/.config/awesome/system-overview"
 #you can set wallpapers in themes as well
 #feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #run applications from startup
