82d0cd321ef047ce8948463aa7835c1aab9f8de4
Wietse de Jong <info@wietsedejong.nl>
2020-10-29 09:16:58 +0100

    Add move tag to next of previous client and switch to it in row 382

--- rc.lua
+++ rc.lua
@@ -83,7 +83,7 @@ local filemanager       = "pcmanfm"
 local mailclient        = "geary"
 local mediaplayer       = "vlc"
 --local scrlocker         = "slimlock"
-local terminal          = "termite" 
+local terminal          = "termite"
 --local virtualmachine    = "virtualbox"
 
 -- awesome variables
@@ -378,6 +378,36 @@ awful.key({ modkey, altkey }, "4", function () awful.util.spawn( "terminator -e 
               end,
               {description = "restore minimized", group = "client"}),
 
+
+-- Mod+ Shift +Left/Right: move client to prev/next tag and switch to it
+              awful.key({ modkey, "Shift" }, "Left",
+                  function ()
+                      -- get current tag
+                      local t = client.focus and client.focus.first_tag or nil
+                      if t == nil then
+                          return
+                      end
+                      -- get previous tag (modulo 9 excluding 0 to wrap from 1 to 9)
+                      local tag = client.focus.screen.tags[(t.name - 2) % 9 + 1]
+                      awful.client.movetotag(tag)
+                      awful.tag.viewprev()
+                  end,
+                      {description = "move client to previous tag and switch to it", group = "layout"}),
+              awful.key({ modkey, "Shift" }, "Right",
+                  function ()
+                      -- get current tag
+                      local t = client.focus and client.focus.first_tag or nil
+                      if t == nil then
+                          return
+                      end
+                      -- get next tag (modulo 9 excluding 0 to wrap from 9 to 1)
+                      local tag = client.focus.screen.tags[(t.name % 9) + 1]
+                      awful.client.movetotag(tag)
+                      awful.tag.viewnext()
+                  end,
+                      {description = "move client to next tag and switch to it", group = "layout"}),
+
+
 -- ALSA volume control
     --awful.key({ modkey1 }, "Up",
     awful.key({ }, "XF86AudioRaiseVolume",
@@ -429,6 +459,8 @@ clientkeys = my_table.join(
         end ,
         {description = "maximize", group = "client"})
 )
+
+
 
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it works on any keyboard layout.
